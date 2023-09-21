29020198a79b52efddf478f8794080c04858cd34
Anxious Ape <iamkhidash@gmail.com>
2023-09-21 14:21:21 +0530

    added food scroll fuctionality and transition

---
+++ app.js
@@ -0,0 +1,17 @@
+//? Get all the icons
+const icons = document.querySelectorAll(".section-1-icons i");
+
+let i = 1;
+
+setInterval(() => {
+  const icon = document.querySelector(".section-1-icons .change");
+  icon.classList.remove("change");
+
+  i++;
+  if (i > icons.length) {
+    icons[0].classList.add("change");
+    i = 1;
+  } else {
+    icon.nextElementSibling.classList.add("change");
+  }
+}, 4000);
